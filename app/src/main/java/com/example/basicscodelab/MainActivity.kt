ab681f8dc558ee640cf8e87f8f7dc28861844850
tayuta914 <tayuta914@gmail.com>
2025-01-28 23:58:54 +0900

    ボタンクリック時にトリガー追加

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +14,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -33,7 +37,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = listOf("World", "Compose"),
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
@@ -44,17 +48,26 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // rememberで再コンポジションの前後で状態を保持するには、remember を使用して可変状態を記憶する
+    var expanded = remember { mutableStateOf(false) }
+    // 各アイテムを個別で展開するように調整
+    var expandPadding = if (expanded.value) 48.dp else 0.dp
     Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Column(
+            modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                .border(2.dp, Color.White)
+        ) {
             Row(modifier = Modifier.padding(24.dp)) {
-                Column(modifier = Modifier.weight(1f)) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = expandPadding)
+                ) {
                     Text(text = "Hello ")
                     Text(text = name)
                 }
-                ElevatedButton(
-                    onClick = { /* TODO */ }
-                ) {
-                    Text(text = "Show more")
+                ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                    Text(if (expanded.value) "Show less" else "Show more")
                 }
             }
         }
